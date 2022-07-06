285f615b64085e4d2603a497453f72138d71f5f9
unknown <james.delaney1251@gmail.com>
2022-07-06 11:54:35 +1000

    remove fluff and add init error checks

--- src/main.c
+++ src/main.c
@@ -1,74 +1,69 @@
 #include <logging/log.h>
 
-LOG_MODULE_REGISTER(main, LOG_LEVEL_DBG);
+LOG_MODULE_REGISTER(main, LOG_LEVEL_ERR);
 
-#include <zephyr.h>
-#include <device.h>
-#include <drivers/gpio.h>
-#include <net/wifi_mgmt.h>
-#include <logging/log_ctrl.h>
+#include <zephyr/zephyr.h>
+#include <zephyr/device.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/net/wifi_mgmt.h>
+#include <zephyr/logging/log_ctrl.h>
 
 #include "io.h"
 #include "msys.h"
 #include "comms_mgr.h"
 #include "settings_util.h"
 
-static struct k_thread main_th;
-
-
-
 void main(void)
 {
     log_init();
     LOG_INF("Starting OWLCMS Referee Controller...\n");
 
-    k_sleep(K_MSEC(1000));
+    // Short startup delay, don't think this is really necessary
+    k_sleep(K_MSEC(100));
+
     int ret = 0;
     ret = io_init();
+    if (ret != 0)
+    {
+        LOG_ERR("Failed to initialise IO module");
+        return;
+    }
+
     ret = settings_util_init();
+    if (ret != 0)
+    {
+        LOG_ERR("Failed to initialise NVS and settings module");
+        return;
+    }
+
     ret = comms_mgr_init(io_get_dev_id());
+    if (ret != 0)
+    {
+        LOG_ERR("Failed to initialise comms module");
+        return;
+    }
 
     ret = msys_init();
-    msys_run();
+    if (ret != 0)
+    {
+        LOG_ERR("Failed to initialise main system module");
+        return;
+    }
 
-    leds_cfg_t leds_cfg;
-
-    // leds_cfg.cfg_type = LED_CFG_TYPE_BLINK_ALT;
-    // leds_cfg.pattern = 0x0A;
-    // io_set_leds_cfg(leds_cfg);
-
-    //LOG_INF("ID of this device is %d", io_get_dev_id());
-
-    /*
-    struct wifi_connect_req_params wifi_cfg;
-    wifi_cfg.ssid_length = 10;
-    settings_util_init();
-    settings_util_load_wifi_config(&wifi_cfg);
-    LOG_INF("ssid len %d", wifi_cfg.ssid_length);
-
-    settings_util_set_wifi_ssid(NULL, 5);
-    settings_util_load_wifi_config(&wifi_cfg);
-    LOG_INF("ssid len %d", wifi_cfg.ssid_length);
-    */
-   
-   /*
-    struct wifi_connect_req_params params;
-    
-    settings_util_load_wifi_config(&params);
-    settings_util_set_wifi_ssid("Delaney", 7);
-
-
-    settings_util_load_wifi_config(&params);
-    settings_util_set_wifi_ssid("NotDelaney", 10);
-    settings_util_load_wifi_config(&params);
-    */
+    ret = msys_run();
+    if (ret != 0)
+    {
+        LOG_ERR("Failed to start main system module");
+        return;
+    }
 
     while(1)
     {
-        //printk("tick\n");
+        // Process buffered log messages and sleep;
         if (log_process() == false)
             k_sleep(K_MSEC(100));
     }
+    // never going to get here...
     LOG_INF("main thread exiting");
 }
 
