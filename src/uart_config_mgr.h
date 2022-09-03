5aca4235175686cd0bff394a05a9ea3d44f93c6a
unknown <james.delaney1251@gmail.com>
2022-09-03 20:56:57 +1000

    fix uart config mgr interface

--- src/uart_config_mgr.h
+++ src/uart_config_mgr.h
@@ -1,14 +1,13 @@
 #ifndef UART_CONFIG_MGR_H_
 #define UART_CONFIG_MGR_H_
 
-#include "settings_util.h"
+int uart_config_mgr_init();
+// // signal to the uart config mgr to enter the config state and start
+// // accepting config commands over uart
+// int uart_config_mgr_start(struct config_settings *settings);
 
-// signal to the uart config mgr to enter the config state and start
-// accepting config commands over uart
-int uart_config_mgr_start(struct config_settings *settings);
-
-// signal to the config mgr to exit the config state and return the 
-// uart to its previous state 
-int uart_config_mgr_stop();
+// // signal to the config mgr to exit the config state and return the 
+// // uart to its previous state 
+// int uart_config_mgr_stop();
 
 #endif
